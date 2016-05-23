ed3098f415458c5071f6e668eb2b761814ae54de
David Robson <David.Robson@AD.Leighton.com>
2016-05-23 18:41:47 +0100

    add functionality

--- src/app/main/controllers/main.controller.js
+++ src/app/main/controllers/main.controller.js
@@ -2,8 +2,21 @@
   'use strict';
 
   angular.module('angularApp')
-    .controller('MainController', function(){
+    .controller('MainController', function($scope){
+      $scope.message = 'this works';
 
-      
-    })
+      $scope.diary = [
+      ];
+
+      $scope.newDiaryEntry = function() {
+        $scope.diary.push({'item':$scope.diaryEntry, 'done':false});
+        $scope.diaryEntry = '';
+      };
+
+      $scope.removeToDo = function() {
+       $scope.todos = $scope.todos.filter(function(item) {
+          return !item.done;
+       })
+    };
+  })
 })();
